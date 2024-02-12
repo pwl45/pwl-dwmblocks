65813e6503a245a592dd7ad740b680d423bdf5bd
Paul Lapey <plapey45@gmail.com>
2024-02-12 10:44:19 -0500

    fix battery script to actually detect not charging

--- bat.sh
+++ bat.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 batpct=$(cat /sys/class/power_supply/BAT0/capacity)
-grep -qi 'charging' /sys/class/power_supply/BAT0/status  && batchg='+' || batchg=''
+grep -qi '^charging' /sys/class/power_supply/BAT0/status  && batchg='+' || batchg=''
 echo "$batpct$batchg"
