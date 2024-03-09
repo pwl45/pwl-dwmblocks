af165be04cc4c37a67b6dd9458c8f1bc531eeb93
Paul Lapey <plapey45@gmail.com>
2024-03-09 16:57:37 -0500

    new battery script to work for laptops with multiple batteries

--- bat.sh
+++ bat.sh
@@ -1,4 +1,28 @@
-#!/usr/bin/env bash
-batpct=$(cat /sys/class/power_supply/BAT0/capacity)
-grep -qi '^charging' /sys/class/power_supply/BAT0/status  && batchg='+' || batchg=''
-echo "$batpct$batchg"
+#!/bin/bash
+
+total_full=0
+total_now=0
+
+for battery in /sys/class/power_supply/BAT*
+do
+    if [ -f "$battery/energy_full" ]; then
+        full=$(cat "$battery/energy_full")
+        now=$(cat "$battery/energy_now")
+    elif [ -f "$battery/charge_full" ]; then
+        full=$(cat "$battery/charge_full")
+        now=$(cat "$battery/charge_now")
+    else
+        echo "Unsupported battery information in $battery"
+        continue
+    fi
+
+    total_full=$((total_full + full))
+    total_now=$((total_now + now))
+done
+
+if [ $total_full -gt 0 ]; then
+    percentage=$((100 * total_now / total_full))
+    echo "$percentage%"
+else
+    echo ""
+fi
